d7eb65e6395e00a7bfac4b418f03292065cce4ba
Sooope <kindraddish@gmail.com>
2025-03-17 21:18:30 +0800

    Added distributed training with parameter serverstrategy
    Code for evaluator is missing, all not tested

---
+++ Models/Classification/param_serv_strat.py
@@ -0,0 +1,110 @@
+#
+# https://www.tensorflow.org/api_docs/python/tf/distribute/Server
+# https://www.tensorflow.org/tutorials/distribute/parameter_server_training#clusters_in_the_real_world
+#
+
+import tensorflow as tf
+import portpicker
+import os
+import json
+from finetunedcnns import getModel
+import load
+import datetime
+
+config = 'TF_CONFIG.json'
+
+weights = None
+
+with open(config, 'r') as src:
+    confjson = json.load(src)
+os.environ["TF_CONFIG"] = json.dumps(confjson)
+
+
+
+
+cluster_resolver = tf.distribute.cluster_resolver.TFConfigClusterResolver()
+if cluster_resolver.task_type in ("worker", "ps"):
+    # Start a TensorFlow server and wait.
+
+    # Set the environment variable to allow reporting worker and ps failure to the
+    # coordinator. This is a workaround and won't be necessary in the future.
+    os.environ["GRPC_FAIL_FAST"] = "use_caller"
+
+    # NOTE: This part might need extra config
+    server = tf.distribute.Server(
+        cluster_resolver.cluster_spec(),
+        job_name=cluster_resolver.task_type,
+        task_index=cluster_resolver.task_id,
+        protocol=cluster_resolver.rpc_layer or "grpc",
+        start=True)
+    server.join()
+
+elif cluster_resolver.task_type == "evaluator":
+    # Run sidecar evaluation
+    # Not written yet
+    pass
+else:
+    # Run the coordinator.
+
+    ds = "Eczema"
+    name = "EfficientNetV2B3"
+    base = "env2b3"
+    Epoches = 10
+    Batch_size = 32
+
+    # Number of PS
+    NUM_PS=1
+
+    # top parameters
+    num_hidden = 1
+    num_neurons = [1000]
+
+    # weights of basemodel
+    weights = None
+
+    variable_partitioner = (
+        tf.distribute.experimental.partitioners.MinSizePartitioner(
+            min_shard_bytes=(256 << 10),
+            max_shards=NUM_PS))
+
+    strategy = tf.distribute.ParameterServerStrategy(
+        cluster_resolver,
+        variable_partitioner=variable_partitioner
+    )
+
+    train_data, val_data, class_names = load.processImage(
+        data_set=ds, batch_size=Batch_size
+    )
+
+    with strategy.scope():
+        model = getModel(
+            class_num=len(class_names), 
+            weights=weights,
+            base=base
+        )
+        model.compile(
+            optimizer="adam",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+
+    working_dir = "tmp/fit/"
+    log_dir = os.path.join(working_dir,"logs/",datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    ckpt_filepath = os.path.join(working_dir, "ckpt")
+    backup_dir = os.path.join(working_dir, "backup")
+
+    # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    callbacks = [
+        tf.keras.callbacks.TensorBoard(log_dir=log_dir),
+        tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_filepath),
+        tf.keras.callbacks.BackupAndRestore(backup_dir=backup_dir),
+    ]
+
+    model.fit(
+        train_data,
+        validation_data=val_data,
+        epochs=Epoches,
+        callbacks=callbacks,
+    )
+    model.save(model, name)
+
